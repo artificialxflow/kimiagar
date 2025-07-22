701f6ac95535fd5018b5c8b1eca0677ef6c2aafa
zedparman <ze.dparman@gmail.com>
2025-07-21 23:17:18 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client']
+  },
+  images: {
+    domains: ['localhost'],
+  },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
 };
 
 export default nextConfig;
