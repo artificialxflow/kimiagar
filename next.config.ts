ecf49782b5e4373c54d9316e90df3a3ddf0ebc9f
zedparman <ze.dparman@gmail.com>
2025-08-25 11:54:00 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,14 @@ const nextConfig: NextConfig = {
   },
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
+    EXTERNAL_PRICE_API_URL: process.env.EXTERNAL_PRICE_API_URL,
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@prisma/client');
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
