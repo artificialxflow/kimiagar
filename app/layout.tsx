6fe8b091f3f2ff31931997a13a2405c6e5e9a5a4
zedparman <ze.dparman@gmail.com>
2025-08-25 09:01:53 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./globals.css";
 import { AuthProvider } from "./components/AuthProvider";
+import { PriceProvider } from "./contexts/PriceContext";
 
 export const metadata = {
   title: "کیمیاگر",
@@ -11,11 +12,13 @@ export default function RootLayout({ children }: React.PropsWithChildren<{}>) {
   return (
     <html lang="fa" dir="rtl">
       <head>
-        <link rel="icon" type="image/svg+xml" href="/app/favicon.svg" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       </head>
       <body style={{ fontFamily: 'Vazirmatn, system-ui, sans-serif', background: '#fff', margin: 0 }}>
         <AuthProvider>
-          {children}
+          <PriceProvider>
+            {children}
+          </PriceProvider>
         </AuthProvider>
       </body>
     </html>
