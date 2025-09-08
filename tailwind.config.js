0a8a3877ac401d50ec608bdba294e8dd9285ce8e
zedparman <ze.dparman@gmail.com>
2025-09-08 02:16:36 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './app/**/*.{js,ts,jsx,tsx}',
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
+      './src/**/*.{js,ts,jsx,tsx}',
+    ],
+  },
   theme: {
     extend: {
       colors: {
