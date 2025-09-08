47dac9c4c67b6e7451980889125dde03d18f3367
zedparman <ze.dparman@gmail.com>
2025-09-08 02:16:49 -0700

    Update route.ts

--- app/api/commissions/route.ts
+++ app/api/commissions/route.ts
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { prisma } from '@/app/lib/prisma';
 
 // دریافت تمام نرخ‌های کارمزد فعال
 export async function GET() {
